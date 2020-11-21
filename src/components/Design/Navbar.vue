94133f7ed0e58f8b94756452530790d83f29b96c
Hillary <66694601+HMoscoso@users.noreply.github.com>
2020-11-21 02:46:40 -0500

    hola

--- src/components/Design/Navbar.vue
+++ src/components/Design/Navbar.vue
@@ -52,6 +52,8 @@ export default {
     FAQ,
     Registrar,
   },
+  
+
 
     data(){
         return{
--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import axios from 'axios';
+import VueGtag from "vue-gtag";
+
+Vue.use(VueGtag, {
+  config: { id: "G-PXDTJ49BGX" }
+});
+
 
 Vue.config.productionTip = false
 axios.defaults.baseURL='http://localhost:54043/'
